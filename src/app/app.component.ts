f6a5829b865a2195f8a1a41eb6159e342f8130e2
laksh bhutani <laksh.bhutani@kelltontech.com>
2020-01-26 20:35:54 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FormList';
+  model: any = {};
+  userList: Array<any> = [
+    {firstName: 'laksh', lastName: 'bhutani', email: 'lakshbhutani@gmail.com' },
+    {firstName: 'aastha', lastName: 'bajaj', email: 'aasthabajaj@gmail.com' },
+  ];
+  // @ViewChild('f') userForm: any;
+  // @ViewChild('f', {static: false}) userForm: any
+  isEditTrue: boolean = false;
+  index;
+
+  onSubmit(f) {
+    this.userList.push(this.model)
+    this.model = { firstName: '', lastName: '', email: ''}
+  }
+
+  onUpdate() {
+    this.userList[this.index] = this.model;
+    this.model = { firstName: '', lastName: '', email: ''}
+    this.isEditTrue = false;
+  }
+
+  onUserEdit(editUser, index) {
+    this.isEditTrue = true;
+    // const item = Object.create(editUser);
+    this.model = editUser;
+    this.index = index;
+  }
+
+  onUserDelete(index) {
+    this.userList.splice(index, 1)
+  }
 }
